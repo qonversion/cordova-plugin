3381b5564210bfff14bee216dc2539ab6d1fb18e
Kamo Spertsyan <spertsiankamo@gmail.com>
2023-08-09 11:28:55 +0300

    Type fix

--- plugin/src/plugin/UserProperties.ts
+++ plugin/src/plugin/UserProperties.ts
@@ -62,7 +62,7 @@ export class UserProperties {
   /**
    * Searches for a property with the given property {@link key} in all properties list.
    */
-  getProperty(key: String): UserProperty | undefined {
+  getProperty(key: string): UserProperty | undefined {
     return this.properties.find(userProperty => userProperty.key == key);
   }
 
