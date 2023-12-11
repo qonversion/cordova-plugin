06543828d91d74fdfa913cd02aacc0ca052c8d88
Kamo Spertsyan <spertsiankamo@gmail.com>
2023-12-11 10:24:46 +0300

    CR fixes

--- plugin/src/plugin/ProductInAppDetails.ts
+++ plugin/src/plugin/ProductInAppDetails.ts
@@ -5,7 +5,7 @@ import {ProductPrice} from "./ProductPrice";
  */
 export class ProductInAppDetails {
   /**
-   * The price of an in-app product.
+   * The price of the in-app product.
    */
   price: ProductPrice;
 
