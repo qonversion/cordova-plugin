7f6a943f63b0dcad26b39e209513f88419417762
Kamo Spertsyan <spertsiankamo@gmail.com>
2024-07-29 15:28:38 +0300

    Release 5.5.2 with Sandwich update to 4.5.2

--- plugin/src/plugin/QonversionInternal.ts
+++ plugin/src/plugin/QonversionInternal.ts
@@ -25,7 +25,7 @@ import {UserProperties} from './UserProperties';
 import {PurchaseModel} from './PurchaseModel';
 import {PurchaseUpdateModel} from './PurchaseUpdateModel';
 
-const sdkVersion = "5.5.1";
+const sdkVersion = "5.5.2";
 
 export default class QonversionInternal implements QonversionApi {
 
