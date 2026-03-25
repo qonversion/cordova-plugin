9429ab9cb0f38c68bd5d761c82822b5d9b734844
suriksarkisyan <suriksarkisyan@users.noreply.github.com>
2026-03-25 11:49:12 +0000

    [create-pull-request] automated change

--- plugin/src/plugin/QonversionInternal.ts
+++ plugin/src/plugin/QonversionInternal.ts
@@ -31,7 +31,7 @@ import {PurchaseOptionsBuilder} from './PurchaseOptionsBuilder';
 import {SKProductDiscount} from './SKProductDiscount';
 import {PromotionalOffer} from './PromotionalOffer';
 
-const sdkVersion = "7.2.1";
+const sdkVersion = "8.4.0";
 
 export default class QonversionInternal implements QonversionApi {
 
