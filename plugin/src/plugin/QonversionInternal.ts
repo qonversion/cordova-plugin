c53150641b717b89ae6a5fc416a04c54259639cc
GitHub Action <action@github.com>
2026-09-23 19:07:32 +0000

    Bump version to 7.11.0

--- plugin/src/plugin/QonversionInternal.ts
+++ plugin/src/plugin/QonversionInternal.ts
@@ -32,7 +32,7 @@ import {PurchaseOptionsBuilder} from './PurchaseOptionsBuilder';
 import {SKProductDiscount} from './SKProductDiscount';
 import {PromotionalOffer} from './PromotionalOffer';
 
-const sdkVersion = "7.10.1";
+const sdkVersion = "7.11.0";
 
 export default class QonversionInternal implements QonversionApi {
 
